2a9f543320c5fc8a2b990125a9de1ea8ce977a1e
fndecroix <fdecroix@eryxsoluciones.com.ar>
2023-04-24 11:05:38 -0300

    Get details of article

--- interfaces/http_interface.py
+++ interfaces/http_interface.py
@@ -1,10 +1,19 @@
+import re
+
+
 class HttpInterface:
     def __init__(self, publishing_system):
         self._publishing_system = publishing_system
 
     def get(self, request):
-        articles = self._publishing_system.published_articles()
-        return HttpResponse(content=articles)
+        path = request['path']
+        if path == '/articles':
+            articles = self._publishing_system.published_articles()
+            return HttpResponse(content=articles)
+        else:
+            matches = re.match(r'/article/(?P<article_id>\d+)$', path)
+            article_id = int(matches['article_id'][0])
+            return HttpResponse(content=self._publishing_system.full_article(article_id))
 
 
 class HttpResponse:
--- tests/test_http_interface.py
+++ tests/test_http_interface.py
@@ -11,16 +11,32 @@ class HttpInterfaceTest(TestCase):
         self.interface = HttpInterface(publishing_system=self.publishing_system)
 
     def test_article_list_when_there_are_no_articles_is_empty(self):
-        self.assertEquals(len(self.interface.get('/articles').content()), 0)
+        self.assertEquals(len(self.interface.get(self._http_request_with_path('/articles')).content()), 0)
 
     def test_can_get_article_list_with_a_single_article(self):
         a_title = 'A title'
-        self.add_an_article_with_title(a_title)
+        a_text = ArticleTexts().valid_text()
+        article_number = self.add_an_article_with(a_title, a_text)
 
-        article_list = self.interface.get('/articles').content()
+        article_list = self.interface.get(self._http_request_with_path('/articles')).content()
 
         self.assertEquals(len(article_list), 1)
-        self.assertEquals(article_list[0]['title'], a_title)
+        self.assertEquals(article_list[0]['number'], article_number)
 
-    def add_an_article_with_title(self, a_title):
-        self.publishing_system.publish_article(a_title, ArticleTexts().valid_text())
+    def test_can_get_details_from_a_single_article(self):
+        a_title = 'A title'
+        a_text = ArticleTexts().valid_text()
+        article_number = self.add_an_article_with(a_title, a_text)
+
+        path = f'/article/{article_number}'
+
+        article_details = self.interface.get(self._http_request_with_path(path)).content()
+
+        self.assertEquals(article_details['text'], a_text)
+        self.assertEquals(article_details['title'], a_title)
+
+    def _http_request_with_path(self, path):
+        return {'path': path}
+
+    def add_an_article_with(self, a_title, a_text):
+        return self.publishing_system.publish_article(a_title, a_text)
