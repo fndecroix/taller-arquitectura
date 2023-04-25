af1e50f0a4742abdc6e54398e1135794d3a3f91e
fndecroix <fdecroix@eryxsoluciones.com.ar>
2023-04-25 12:00:08 -0300

    Usa content dentro de la HTTPInterface que no usa Django

--- tests/http_client.py
+++ tests/http_client.py
@@ -6,11 +6,11 @@ class HttpClient:
         self._interface = interface
 
     def get_articles(self):
-        return self._interface.get_articles(self._http_request_with(parameters={}))
+        return self._interface.get_articles(self._http_request_with(parameters={})).content()
 
     def get_article(self, article_number):
         parameters = {'article_number': article_number}
-        return self._interface.get_article(self._http_request_with(parameters=parameters))
+        return self._interface.get_article(self._http_request_with(parameters=parameters)).content()
 
     def _http_request_with(self, parameters):
         return HttpRequest(parameters=parameters)
--- tests/test_http_interface.py
+++ tests/test_http_interface.py
@@ -12,14 +12,14 @@ class HttpInterfaceTest(TestCase):
         self.http_client = HttpClient(HttpInterface(publishing_system=self.publishing_system))
 
     def test_article_list_when_there_are_no_articles_is_empty(self):
-        self.assertEquals(len(self.http_client.get_articles().content()), 0)
+        self.assertEquals(len(self.http_client.get_articles()), 0)
 
     def test_can_get_article_list_with_a_single_article(self):
         a_title = 'A title'
         a_text = ArticleTexts().valid_text()
         article_number = self.add_an_article_with(a_title, a_text)
 
-        article_list = self.http_client.get_articles().content()
+        article_list = self.http_client.get_articles()
 
         self.assertEquals(len(article_list), 1)
         self.assertEquals(article_list[0]['number'], article_number)
@@ -29,7 +29,7 @@ class HttpInterfaceTest(TestCase):
         a_text = ArticleTexts().valid_text()
         article_number = self.add_an_article_with(a_title, a_text)
 
-        article_details = self.http_client.get_article(article_number).content()
+        article_details = self.http_client.get_article(article_number)
 
         self.assertEquals(article_details['text'], a_text)
         self.assertEquals(article_details['title'], a_title)
