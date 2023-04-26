d7a1a5be89ee1ee32c8de4196daf57a5ea1cee26
fndecroix <fdecroix@eryxsoluciones.com.ar>
2023-04-26 11:41:06 -0300

    Decoupling django step 2: adding urls
    Al settings.configure le tengo que pasar un parametro con cada variable que hay en settings.
    
    ROOT_URLCONF tiene una ruta a un archivo de urls, pero le puedo pasar un objeto que tenga una property 'urlpatterns'

--- publishing_system/tests/django_http_client.py
+++ publishing_system/tests/django_http_client.py
@@ -1,5 +1,6 @@
 import json
 
+from django.conf import settings
 from django.test.client import Client
 from django.urls import reverse
 
@@ -7,6 +8,9 @@ from publishing_system.publishing_system.global_publishing_system import GlobalP
 
 
 class DjangoHttpClient:
+    def __init__(self):
+        settings.configure(ROOT_URLCONF=UrlConf())
+
     def set_system(self, new_system):
         GlobalPublishingSystem.set_system(new_system)
 
@@ -17,3 +21,19 @@ class DjangoHttpClient:
 
     def django_client(self):
         return Client()
+
+
+class UrlConf:
+    @property
+    def urlpatterns(self):
+        from django.contrib import admin
+        from django.urls import path
+
+        from publishing_system.publishing_system.views.publishing_system_views import PublishingSystemViews
+
+        urlpatterns = [
+            path('admin/', admin.site.urls),
+            path('articles/', PublishingSystemViews().get_articles, name='list-articles'),
+        ]
+
+        return urlpatterns
