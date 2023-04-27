62798388286c51a66723fdd43f9140de605bb4dd
fndecroix <fdecroix@eryxsoluciones.com.ar>
2023-04-27 11:16:58 -0300

    Agrego secret key a las settings

--- publishing_system/manage.py
+++ publishing_system/manage.py
@@ -9,7 +9,8 @@ def main():
     from system import PublishingSystem
     from django_framework.django_http_client import UrlConf
 
-    settings.configure(ROOT_URLCONF=UrlConf(PublishingSystem()), DEBUG=True)
+    secret_key = 'django-insecure-^3#otl^ose7xu_t8wyt5*vxlcdp%cse6oobxz94&j19p0&tc!h'
+    settings.configure(ROOT_URLCONF=UrlConf(PublishingSystem()), DEBUG=True, SECRET_KEY=secret_key)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
