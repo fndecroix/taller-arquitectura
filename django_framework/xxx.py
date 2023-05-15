c59de618fa8303a619f10be1104dc8f639f1f2f4
fndecroix <fdecroix@eryxsoluciones.com.ar>
2023-05-15 10:29:50 -0300

    Extract settings config

--- django_framework/xxx.py
+++ django_framework/xxx.py
@@ -5,17 +5,8 @@ class Xxx:
 
     def initialize_django_from_managepy(self, assistant):
         from django.conf import settings
-        settings.configure(
-            DEBUG=True,
-            SECRET_KEY="django-insecure-!l2bq7t!&iyafejbpwfefwdao$j#us@ygq&m27uct3=b#@k6f8",
-            INSTALLED_APPS=["publishing_system"],
-            DATABASES={
-                'default': {
-                    'ENGINE': 'django.db.backends.sqlite3',
-                    'NAME': 'db.sqlite3',
-                }
-            }
-        )
+        configuration = self.django_settings_configuration()
+        settings.configure(**configuration)
 
         import django
         django.setup()
@@ -28,19 +19,7 @@ class Xxx:
         settings_manager.enable()
 
     def initialize_django_from_http_client(self, assistant):
-        from django.conf import settings
-        if not settings.configured:
-            settings.configure(
-                DEBUG=True,
-                SECRET_KEY="django-insecure-!l2bq7t!&iyafejbpwfefwdao$j#us@ygq&m27uct3=b#@k6f8",
-                INSTALLED_APPS=["publishing_system"],
-                DATABASES={
-                    'default': {
-                        'ENGINE': 'django.db.backends.sqlite3',
-                        'NAME': 'db.sqlite3',
-                    }
-                }
-            )
+        self.configure_settings()
         from django.test.utils import override_settings
         from django_framework.url_conf import UrlConf
         settings_manager = override_settings(ROOT_URLCONF=UrlConf(assistant))
@@ -50,19 +29,7 @@ class Xxx:
         # django.setup()
 
     def initialize_django_from_test(self):
-        from django.conf import settings
-        if not settings.configured:
-            settings.configure(
-                DEBUG=True,
-                SECRET_KEY="django-insecure-!l2bq7t!&iyafejbpwfefwdao$j#us@ygq&m27uct3=b#@k6f8",
-                INSTALLED_APPS=["publishing_system"],
-                DATABASES={
-                    'default': {
-                        'ENGINE': 'django.db.backends.sqlite3',
-                        'NAME': 'db.sqlite3',
-                    }
-                }
-            )
+        self.configure_settings()
 
         import django
         django.setup()
@@ -80,3 +47,23 @@ class Xxx:
         from django.test.runner import DiscoverRunner
         discovery = DiscoverRunner()
         discovery.teardown_databases(self._db_config)
+
+    def configure_settings(self):
+        from django.conf import settings
+        if not settings.configured:
+            configuration = self.django_settings_configuration()
+            settings.configure(**configuration)
+
+    def django_settings_configuration(self):
+        configuration = {
+            'DEBUG': True,
+            'SECRET_KEY': "django-insecure-!l2bq7t!&iyafejbpwfefwdao$j#us@ygq&m27uct3=b#@k6f8",
+            'INSTALLED_APPS': ["publishing_system"],
+            'DATABASES': {
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': 'db.sqlite3',
+                }
+            }
+        }
+        return configuration
